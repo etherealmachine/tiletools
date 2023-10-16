caf9eb15e0c6f46bc22096cb0c927078715c997a
James Pettit <etherealmachine@gmail.com>
2023-10-15 20:06:14 -0700

    More build fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
+    import { base } from "$app/paths";
 </script>
 
 <div style="display: flex; flex-direction: column; align-items: center; width: 100%;">
   <div style="width: 1040px; max-width: 1040px; display: flex; flex-direction: column; align-items: center;">
     <h1>Welcome to the Weave!</h1>
     <div style="display: grid; grid-template-columns: 1fr 1fr; gap: 32px; margin: auto;">
-      <a href="/map-editor" style="flex-grow: 1" class="button">Map Editor</a>
-      <a href="/tileset-editor" class="button">Tileset Editor</a>
-      <a href="/merge-tilesets" class="button">Merge Tilesets</a>
-      <a href="/about" class="button">About</a>
+      <a href="{base}/map-editor" style="flex-grow: 1" class="button">Map Editor</a>
+      <a href="{base}/tileset-editor" class="button">Tileset Editor</a>
+      <a href="{base}/merge-tilesets" class="button">Merge Tilesets</a>
+      <a href="{base}/about" class="button">About</a>
     </div>
   </div>
 </div>
